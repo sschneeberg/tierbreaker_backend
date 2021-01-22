87047095b94e0aad66472d8dac34cc4d28ea1c74
sschneeberg <simone.m.schneeberg@gmail.com>
2021-01-21 20:18:15 -0500

    test voting

--- server.py
+++ server.py
@@ -37,29 +37,32 @@ def all_public_brackets():
 def show_bracket(bracket_key):
     # query db for specific bracket to vote
     # either end display or voting options
-    bracket = Bracket.objects(key=bracket_key)[0]
+    bracket = Bracket.objects(key=bracket_key)[0] # returns only one 
     return jsonify(bracket)
 
 @app.route('/bracket/<bracket_key>/vote', methods=['PUT'])
 def add_vote(bracket_key):
     # query db for bracket key, request.json['option']
     option = request.json['option']
-    bracket = Bracket.objects(key=bracket_key)
-    round_votes = bracket.voting_options__votes
+    bracket = Bracket.objects(key=bracket_key)[0]
+    round_votes = bracket.voting_options.votes
     round_votes[-1][option] += 1
-    total_votes = bracket.voting_options__totals
+    total_votes = bracket.voting_options.totals
     total_votes[option] += 1
-    bracket.update_one(set__voting_options__votes=round_votes, set__voting_options__totals=total_votes)
-    return
+    Bracket.objects(id=bracket.id).update_one(set__voting_options__votes=round_votes, set__voting_options__totals=total_votes)
+    bracket.reload()
+    return { "vote": f"bracket option: {option} updated", "bracket": bracket}
 
 @app.route('/bracket/create', methods=['POST'])
 def create_bracket():
     # bracket = Bracket(key, title, time_duration, num_rounds, round_duration,created_at, end_display_formation, private, voting_options)
     
     def gen_key():
-        key = uuid4().hex[:8]
-        if Keys.objects(key=key): return gen_key()
-        return key
+        poll_key = uuid4().hex[:8]
+        if Keys.objects(key=poll_key): return gen_key()
+        new_key = Keys(key=poll_key)
+        new_key.save()
+        return poll_key
 
     bracket = Bracket()
     bracket.title = request.json['title']
@@ -82,10 +85,9 @@ def create_bracket():
     options.round_options = request.json['options_list']
     options.votes = [gen_votes(request.json['options_list'])]
     options.totals = gen_votes(request.json['options_list'])
-
     bracket.voting_options = options
-
     bracket.save()
+
     return { "msg" : "bracket created", "bracket" : bracket }
 
 @app.route('/bracket/<bracket_key>/edit', methods=['PUT'])
@@ -95,7 +97,7 @@ def update_duration(bracket_key):
     # if not working just make it total rounds
     new_round_dur = new_dur // (bracket.num_rounds - len(bracket.voting_options.votes))
     bracket.update_one(set__time_duration=new_dur, set__round_duration=new_round_dur)
-    return { "msg" : "duration updated"}
+    return { "msg" : "duration updated" }
 
 @app.route('/bracket/<bracket_key>/tally', methods=['PUT'])
 def tally_votes(bracket_key):
--- testing/test/server.test.js
+++ testing/test/server.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const assert = require('assert');
+const axios = require('axios');
 
 describe('Server', function () {
     describe('GET /ping', function () {
@@ -41,7 +42,7 @@ describe('Bracket Routes', function () {
 
     describe('GET /brackets/<bracket_key>', function () {
         it('should grab a single bracket for the given key', function (done) {
-            key = '57000d62';
+            const key = '57000d62';
             request('http://localhost:8000')
                 .get(`/bracket/${key}`)
                 .expect(200)
@@ -56,14 +57,14 @@ describe('Bracket Routes', function () {
     });
 
     // describe('POST /bracket/create', function () {
-    //     it('should return build a bracket', function (done) {
+    //     it('should build a bracket', function (done) {
     //         const opts = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight'];
     //         request('http://localhost:8000')
     //             .post('/bracket/create')
     //             .send({
     //                 options_list: opts,
     //                 duration: '4',
-    //                 title: 'Who would win TWO',
+    //                 title: 'Who would win ?',
     //                 num_options: '8',
     //                 private: true,
     //                 end_display: 'Winner'
@@ -75,7 +76,7 @@ describe('Bracket Routes', function () {
     //                 assert(bracket.num_rounds === Math.log2(8), true);
     //                 assert(bracket.time_duration === 4, true);
     //                 assert(bracket.round_duration === parseInt(4 / Math.log2(8)), true);
-    //                 assert(bracket.title === 'Who would win TWO', true);
+    //                 assert(bracket.title === 'Who would win ?', true);
     //                 assert(bracket.end_display_format === 'Winner', true);
     //                 assert(bracket.private === true, true);
     //                 for (let i = 0; i < bracket.voting_options.round_options.length; i++) {
@@ -88,5 +89,57 @@ describe('Bracket Routes', function () {
     //             })
     //             .catch((err) => done(err));
     //     });
-    // });
+
+    // WRITE THIS TEST LATER, NOT VITAL FOR MVP FUNCTIONALITY
+    // it('should not have a duplicate key', function () {
+    //     const opts = ['one', 'two', 'three', 'four'];
+    //     request('http://localhost:8000')
+    //         .post('/bracket/create')
+    //         .send({
+    //             options_list: opts,
+    //             duration: '4',
+    //             title: 'Who would win 2.0',
+    //             num_options: '8',
+    //             private: false,
+    //             end_display: 'Full'
+    //         })
+    //         .expect(200)
+    //         .expect('Content-Type', /json/)
+    //         .then((response) => {
+    //             bracket = response.body.bracket;
+    //             assert(bracket.num_rounds === Math.log2(8), true);
+    //               // query keys from db and make sure it's not there
+    //             done();
+    //         })
+    //         .catch((err) => done(err));
+    // })
+
+    describe('PUT /bracket/<bracket_key>/vote', function () {
+        it('should increment the vote for the proper option by one', function (done) {
+            const key = '57000d62';
+            const option = 'one';
+            axios
+                .get(`http://localhost:8000/bracket/${key}`)
+                .then((prevVotes) => {
+                    request('http://localhost:8000')
+                        .put(`/bracket/${key}/vote`)
+                        .send({ option: option })
+                        .expect(200)
+                        .expect('Content-Type', /json/)
+                        .then((response) => {
+                            bracketVoting = response.body.bracket.voting_options;
+                            prevVoting = prevVotes.data.voting_options;
+                            assert(
+                                bracketVoting.votes[bracketVoting.votes.length - 1][option] ===
+                                    prevVoting.votes[prevVoting.votes.length - 1][option] + 1
+                            );
+
+                            assert(bracketVoting.totals[option] === prevVoting.totals[option] + 1);
+                            done();
+                        })
+                        .catch((err) => done(err));
+                })
+                .catch((err) => done(err));
+        });
+    });
 });
